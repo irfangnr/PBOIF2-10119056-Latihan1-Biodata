2d627ecf7c48ab8e0dad953a5c0d3d6fa764c736
ACER <ACER@Irfan>
2020-10-16 16:56:50 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan1/biodata/PBOIF210119056Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+
+package pboif2.pkg10119056.latihan1.biodata;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan biodata
+ */
+public class PBOIF210119056Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCSTAR" + "\n" + "---------------------------------");
+        System.out.println("NAMA\t\t: Irfan Ginanjar");
+        System.out.println("ALAMAT\t\t: Kp. Manggahang");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 27 AGUSTUS 2000");
+        System.out.println("---------------------------------");
+        System.out.println("HOBI\t\t: Bersepeda");
+        System.out.println("MAKES\t\t: Mie");
+        System.out.println("MIKES\t\t: Air Putih");
+    }
+    
+}
